787f2381f7849810bcaa80536b0ebcf5097f7596
PritamKumarSaha <137249952+PritamKumarSaha@users.noreply.github.com>
2023-12-17 23:32:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let totalCost =0;
+	arr.sort((a,b) => b-a);
+	while(arr.length>1){
+		let currentCost = arr[arr.length-1] + arr[arr.length -2];
+		totalCost+=currentCost;
+		arr.pop(); arr.pop();
+		arr.push(currentCost);
+		arr.sort((a,b) => b-a);
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
